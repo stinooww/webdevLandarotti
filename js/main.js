aa309c8cfb25e09e8e02397a3e6bd517fdde287d
stijn heynderickx <stijn.heynderickx@gmail.com>
2018-01-18 18:33:07 +0100

    add new auction  -- start form validatie

--- js/main.js
+++ js/main.js
@@ -1,5 +1,53 @@
+/*Deze functie controlleert al de inputs of ze leeg zijn of niet*/
+function send(e) {
+    e.preventDefault();
+    var dropdownList =  document.getElementById("ddl");
+    var selectDdl = dropdownList.options[dropdownList.selectedIndex].value;
+    var title = document.getElementById("title");
+    var titleLen = title.length;
+    var year = document.getElementById( "year");
+    var yearLen= year.length;
+    var width = document.getElementById( "width");
+    var widthLen = width.length;
+    var height = document.getElementById( "height");
+    var heightLen = height.length;
+    var depth = document.getElementById( "depth");
+    var depthLen = depth.length;
+    var description = document.getElementById( "description");
+    var descriptionLen =  description.length;
+    var origin = document.getElementById( "origin");
+    var originLen = origin.length;
+    var image = document.getElementById( "image");
+    var imageLen =  image.length;
+    var signature = document.getElementById( "signature");
+    var signatureLen =  signature.length;
+    var optional = document.getElementById( "optional");
+    var optionalLen = optional.length;
+    var minimum = document.getElementById( "minimum");
+    var minimumLen = minimum.length;
+    var maximum = document.getElementById( "maximum");
+    var maximumLen = maximum.length;
+    var price = document.getElementById( "price");
+    var priceLen = price.length;
+    var end = document.getElementById( "end");
+    var endLen = end.length;
+    var check = document.getElementById( "check");
+    var checkLen
+    var isValid = false;
+
+if(selectDdl == 0){
+    console.log('leeg');
+}
+
+}
+
+
+
+
+/**function bindEvents: functie  die alle event handlers  aan de functies verbind */
 function bindEvents() {
-    
+    var formbtn = document.getElementById("addNewAuc");
+    formbtn.addEventListener('submit', send);
 }
 
 bindEvents();
